91e64a07fd67702870d3fc8e811b38593d3dd297
HiroakiKonno <sample@sample.co.jp>
2021-05-23 06:09:36 +0000

    Finish login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
    @user = User.new(user_params)
     if @user.save
       flash[:success] = '新規作成に成功しました。'
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
+      redirect_to root_url
     else
       render :new
     end
