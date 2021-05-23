6cc6d0d9592639452474412afea1055c64395311
HiroakiKonno <sample@sample.co.jp>
2021-05-23 03:18:50 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+   @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end
+  
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :belonging, :password, :password_confirmation)
+    end
+
 end
