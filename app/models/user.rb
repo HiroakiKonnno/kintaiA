9b61eaaa63afef2eebe0bdad942e13e25dc041b2
HiroakiKonno <sample@sample.co.jp>
2021-05-24 12:06:19 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,33 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+# 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
