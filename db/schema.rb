9ca332215b5b56c85dcba5f979e103c93b713e99
HiroakiKonno <sample@sample.co.jp>
2021-05-27 23:04:18 +0000

    Finish editing-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210524104249) do
+ActiveRecord::Schema.define(version: 20210527131429) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20210524104249) do
     t.string "password_digest"
     t.string "belonging"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
